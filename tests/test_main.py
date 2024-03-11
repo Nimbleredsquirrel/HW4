7e62b5fec57ed71f2c6aca705413b394434be83c
Arina Zamyshevskaya <90250718+Nimbleredsquirrel@users.noreply.github.com>
2024-03-11 22:36:41 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,32 @@
 import pytest
 from fastapi.testclient import TestClient
 
+from testcontainers.postgres import PostgresContainer
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.models import Base
+
 from app.main import app
+
+postgres = PostgresContainer("postgres:13.3")
+postgres.start()
+engine = create_engine(postgres.get_connection_url())
+
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine
+)
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
