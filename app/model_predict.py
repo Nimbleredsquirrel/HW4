499beb98b88895767be5c3f1fd2b1c8a02759b2f
Arina Zamyshevskaya <90250718+Nimbleredsquirrel@users.noreply.github.com>
2024-03-12 00:31:18 +0300

    Update model_predict.py

--- app/model_predict.py
+++ app/model_predict.py
@@ -5,7 +5,7 @@ import requests
 def tree_count(town, year):
     """Parser for OSM."""
     url = r"http://overpass-api.de/api/interpreter"
-    query = f"""[out:json][date:'{year}-01-01T00:00:00Z'][maxsize:2000000000];
+    query = f"""[out:json][date:'{str(year)}-01-01T00:00:00Z'][maxsize:2000000000];
     area["name:en"="{town}"]->.searchArea;
     (node["natural"="tree"](area.searchArea);); out count;"""
     result = requests.post(url, data=query, timeout=60)
